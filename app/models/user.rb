ef727f5fadec97386a49482317bad490ea22c374
Belinschi Dima <belinski.d@gmail.com>
2018-03-17 16:24:00 +0200

    unique identifier for customer

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   private
 
   def create_customer
+    identifier = Digest::MD5.hexdigest("#{SecureRandom.hex(12)}-#{email}")
+                          .chars.sample(12).join("")
+                          .gsub(/(.)(.?)/) { Regexp.last_match(1).upcase + Regexp.last_match(2).downcase }
+
     client = Saltedge.new(ENV["SALTEDGE_ID"], ENV["SALTEDGE_SECRET"])
     path = ENV["API_ROOT"] + "customers"
-    params = { data: { identifier: email } }
+    params = { data: { identifier: identifier } }
     data = client.request("POST", path, params)
   end
 end
